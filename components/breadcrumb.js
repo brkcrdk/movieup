62117d7901f346afe1535c846790ed52bbc8ad5e
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-11-20 01:01:39 +0300

    breadcrumb updated.

--- components/breadcrumb.js
+++ components/breadcrumb.js
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
+import Link from 'next/link';
+import Container from '~/components/container';
 import { color } from '~/theme';
 
 const Breadcrumb = () => {
   return (
     <StyledBreadcrumb>
-      <p>Breadcrumb is here</p>
+      <Container>
+        <Link href="/">
+          <a>Home /</a>
+        </Link>
+        <Link href="/">
+          <a>Search Result</a>
+        </Link>
+      </Container>
     </StyledBreadcrumb>
   );
 };
@@ -13,4 +22,19 @@ export default Breadcrumb;
 
 const StyledBreadcrumb = styled.div`
   background: ${color.breadcrumbBackground};
+  padding: 24px 0;
+  align-items: center;
+  a {
+    margin: 0 5px;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 30px;
+    &:hover {
+      color: ${color.yellow};
+    }
+  }
+
+  .container {
+    display: flex;
+  }
 `;
