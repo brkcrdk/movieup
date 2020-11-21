be9ee1dd8338c57ee0031f864a91ac374553be4a
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-11-21 13:52:34 +0300

    Initial loading page

--- components/loading.js
+++ components/loading.js
@@ -1,6 +1,13 @@
 const Loading = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh', display: 'grid' }}>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'grid',
+        placeItems: 'center',
+      }}
+    >
       <img src="/static/images/logo.png" />
       <h1 style={{ color: '#F5C518', marginTop: 30, fontSize: 25 }}>
         Loading.. Please wait.
