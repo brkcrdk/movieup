b1ae789f6d281c2abc9ff99f6fe861ba0731be7b
burak cardak <burak.cardak@fabrikamedya.com.tr>
2020-11-21 13:23:28 +0300

    Title updated.

--- layout/head.js
+++ layout/head.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 const head = () => {
   return (
     <Head>
+      <title>Movie UP</title>
       <link rel="stylesheet" href="/static/styles/default.css" />
       <link rel="stylesheet" href="/static/styles/normalize.css" />
       <link rel="stylesheet" href="/static/icomoon/style.css" />
